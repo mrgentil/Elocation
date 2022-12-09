0d464b6ccccc77365e246adcacda8055273b22e2
mrgentil <tshitshob@gmail.com>
2022-12-09 19:05:02 +0100

    authentifications

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,220 +1,92 @@
 <!DOCTYPE html>
-
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
-    <link rel="stylesheet" href="{{asset("build/assets/app.962ef7cc.css")}}">
-    <script nonce="2732c0cd-9ad9-4bb7-bf93-c32108c95473">(function (w, d) {
-            !function (cM, cN, cO, cP) {
-                cM.zarazData = cM.zarazData || {};
-                cM.zarazData.executed = [];
-                cM.zaraz = {deferred: [], listeners: []};
-                cM.zaraz.q = [];
-                cM.zaraz._f = function (cQ) {
-                    return function () {
-                        var cR = Array.prototype.slice.call(arguments);
-                        cM.zaraz.q.push({m: cQ, a: cR})
-                    }
-                };
-                for (const cS of ["track", "set", "debug"]) cM.zaraz[cS] = cM.zaraz._f(cS);
-                cM.zaraz.init = () => {
-                    var cT = cN.getElementsByTagName(cP)[0], cU = cN.createElement(cP),
-                        cV = cN.getElementsByTagName("title")[0];
-                    cV && (cM.zarazData.t = cN.getElementsByTagName("title")[0].text);
-                    cM.zarazData.x = Math.random();
-                    cM.zarazData.w = cM.screen.width;
-                    cM.zarazData.h = cM.screen.height;
-                    cM.zarazData.j = cM.innerHeight;
-                    cM.zarazData.e = cM.innerWidth;
-                    cM.zarazData.l = cM.location.href;
-                    cM.zarazData.r = cN.referrer;
-                    cM.zarazData.k = cM.screen.colorDepth;
-                    cM.zarazData.n = cN.characterSet;
-                    cM.zarazData.o = (new Date).getTimezoneOffset();
-                    if (cM.dataLayer) for (const cZ of Object.entries(Object.entries(dataLayer).reduce(((c_, da) => ({...c_[1], ...da[1]}))))) zaraz.set(cZ[0], cZ[1], {scope: "page"});
-                    cM.zarazData.q = [];
-                    for (; cM.zaraz.q.length;) {
-                        const db = cM.zaraz.q.shift();
-                        cM.zarazData.q.push(db)
-                    }
-                    cU.defer = !0;
-                    for (const dc of [localStorage, sessionStorage]) Object.keys(dc || {}).filter((de => de.startsWith("_zaraz_"))).forEach((dd => {
-                        try {
-                            cM.zarazData["z_" + dd.slice(7)] = JSON.parse(dc.getItem(dd))
-                        } catch {
-                            cM.zarazData["z_" + dd.slice(7)] = dc.getItem(dd)
-                        }
-                    }));
-                    cU.referrerPolicy = "origin";
-                    cU.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(cM.zarazData)));
-                    cT.parentNode.insertBefore(cU, cT)
-                };
-                ["complete", "interactive"].includes(cN.readyState) ? zaraz.init() : cM.addEventListener("DOMContentLoaded", zaraz.init)
-            }(w, d, 0, "script");
-        })(window, document);
-    </script>
+    <title>E-MANAGE</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     @livewireStyles
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
 
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="far fa-bell"></i>
-                    <span class="badge badge-warning navbar-badge">15</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                    <span class="dropdown-header">15 Notifications</span>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fas fa-envelope mr-2"></i> 4 new messages
-                        <span class="float-right text-muted text-sm">3 mins</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                    <i class="fas fa-expand-arrows-alt"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                    <i class="fas fa-user"></i>
-                </a>
-            </li>
-        </ul>
-    </nav>
+    <!-- Navbar -->
+    <x-topnav />
+    <!-- /.navbar -->
 
-
+    <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-
-        <a href="" class="brand-link">
-            <span class="brand-text font-weight-bold" style="font-size: 1.3em;"><b>My Management</b></span>
+        <!-- Brand Logo -->
+        <a href="index3.html" class="brand-link">
+            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light" style="font-size: 1.3em;"><b>E- MANAGE</b></span>
         </a>
 
+        <!-- Sidebar -->
         <div class="sidebar">
-
+            <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="{{asset('user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
+                    <a href="#" class="d-block">{{userFullName()}}</a>
                 </div>
             </div>
 
-            <div class="form-inline">
-                <div class="input-group" data-widget="sidebar-search">
-                    <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                           aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-sidebar">
-                            <i class="fas fa-search fa-fw"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <!-- SidebarSearch Form -->
 
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                    data-accordion="false">
 
-                    <li class="nav-item menu-open">
-                        <a href="#" class="nav-link active">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
-                            <p>
-                                Starter Pages
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link active">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Active Page</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Inactive Page</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-th"></i>
-                            <p>
-                                Simple Link
-                                <span class="right badge badge-danger">New</span>
-                            </p>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
+            <!-- Sidebar Menu -->
+            <x-menu />
+            <!-- /.sidebar-menu -->
         </div>
-
+        <!-- /.sidebar -->
     </aside>
 
+    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Starter Page</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Starter Page</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
+        <!-- Main content -->
         <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-
-
-                </div>
-
-            </div>
+          <div class="container-fluid">
+              @yield("content")
+          </div>
+            <!-- /.container-fluid -->
         </div>
-
+        <!-- /.content -->
     </div>
+    <!-- /.content-wrapper -->
 
+    <!-- Control Sidebar -->
+    <x-sidebar />
+    <!-- /.control-sidebar -->
 
-    <aside class="control-sidebar control-sidebar-dark">
-
-        <div class="p-3">
-            <h5>Title</h5>
-            <p>Sidebar content</p>
-        </div>
-    </aside>
-
-
+    <!-- Main Footer -->
     <footer class="main-footer">
-
+        <!-- To the right -->
         <div class="float-right d-none d-sm-inline">
             Anything you want
         </div>
-
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <!-- Default to the left -->
+        <strong>Copyright &copy; {{date('d-m-y')}} <a href="https://github.com/mrgentil">MrGentil</a>.</strong> All rights reserved.
     </footer>
 </div>
-<script src="{{asset("build/assets/app.5dcd57a4.js")}}"></script>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
 @livewireScripts
 </body>
 </html>
